1217d7e98f64525c5669cd582ea352ed0663db42
ramya <nsriramya7@gmailcom>
2024-12-20 15:43:20 +0530

    done

--- src/utils/ApiRoutes.js
+++ src/utils/ApiRoutes.js
@@ -1,4 +1,4 @@
-export const HOST = "http://localhost:3005";
+export const HOST = "https://tele-dailer-backend.vercel.app";
 
 const AUTH_ROUTE= `${HOST}/api/auth`;
 const MESSAGES_ROUTE= `${HOST}/api/messages`;
